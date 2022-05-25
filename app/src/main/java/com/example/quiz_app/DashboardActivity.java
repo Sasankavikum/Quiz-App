8fe756056493003c8c6b49907ec6f549c2f521af
Sasankavikum <sasavikum@gmail.com>
2022-05-25 13:22:37 +0530

    dashboard complete

--- app/src/main/java/com/example/quiz_app/DashboardActivity.java
+++ app/src/main/java/com/example/quiz_app/DashboardActivity.java
@@ -2,13 +2,36 @@ package com.example.quiz_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    CountDownTimer countDownTimer;
+    int timeValue = 20;
+    LinearProgressIndicator progressIndicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        progressIndicator=findViewById(R.id.timer);
+
+        countDownTimer = new CountDownTimer(20000,1000) {
+            @Override
+            public void onTick(long l) {
+                timeValue=timeValue-1;
+                progressIndicator.setProgress(timeValue);
+            }
+
+            @Override
+            public void onFinish() {
+                Dialog dialog = new Dialog(DashboardActivity.this);
+            }
+        }.start();
     }
 }
--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this,DashboardActivity.class);
+                startActivity(intent);
             }
         },1500);
 
