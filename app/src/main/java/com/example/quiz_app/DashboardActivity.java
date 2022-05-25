dd1ee06af08e846be7d4094f0d5954549479044b
Sasankavikum <sasavikum@gmail.com>
2022-05-25 20:52:33 +0530

    update dashboard

--- app/src/main/java/com/example/quiz_app/DashboardActivity.java
+++ app/src/main/java/com/example/quiz_app/DashboardActivity.java
@@ -3,15 +3,17 @@ package com.example.quiz_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.view.View;
 
 import com.google.android.material.progressindicator.LinearProgressIndicator;
 
 public class DashboardActivity extends AppCompatActivity {
 
     CountDownTimer countDownTimer;
-    int timeValue = 20;
+    int timeValue = 30;
     LinearProgressIndicator progressIndicator;
 
     @Override
@@ -21,7 +23,7 @@ public class DashboardActivity extends AppCompatActivity {
 
         progressIndicator=findViewById(R.id.timer);
 
-        countDownTimer = new CountDownTimer(20000,1000) {
+        countDownTimer = new CountDownTimer(30000,1000) {
             @Override
             public void onTick(long l) {
                 timeValue=timeValue-1;
@@ -29,11 +31,20 @@ public class DashboardActivity extends AppCompatActivity {
             }
 
             @Override
+            
             public void onFinish() {
                 Dialog dialog = new Dialog(DashboardActivity.this,R.style.Dialog);
                 dialog.setContentView(R.layout.activity_time_out);
+                dialog.findViewById(R.id.btntryagain).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(DashboardActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }
+                });
                 dialog.show();
             }
+
         }.start();
     }
 }
